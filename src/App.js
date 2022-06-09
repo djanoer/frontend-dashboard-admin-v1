0cdea7fcdba266492b3a8ec6091f44308662bad4
dave_id <djanur405@gmail.com>
2022-06-09 12:03:36 +0800

    create sidebar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar,Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages';
 import './App.css';
 
 const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
 
   return (
     <div>
@@ -63,7 +63,7 @@ const App = () => {
               <Route path='/color-picker' element={<ColorPicker />} />
               
               {/* Charts */}
-              <Route path='/line' element={<Kanban />} />
+              <Route path='/line' element={<Line />} />
               <Route path='/area' element={<Area />} />
               <Route path='/bar' element={<Bar />} />
               <Route path='/pie' element={<Pie />} />
