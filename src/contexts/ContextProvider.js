55361641d24546b09fac579b8f3e4b7d45a698da
dave_id <djanur405@gmail.com>
2022-06-21 11:48:31 +0800

    kanban

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    useProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+
+    const handleCLick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
+    return (
+        <StateContext.Provider
+            value={{
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleCLick,
+                screenSize,
+                setScreenSize
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
+
