72c1a20298715661bf35355fe19df52654402e94
dave_id <djanur405@gmail.com>
2022-06-22 15:32:42 +0800

    craete chart and update file sparkline

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize, currentColor } = useStateContext();
 
   const handleCloseSidebar = () => {
     if(activeMenu && screenSize <= 900) {
@@ -47,6 +47,10 @@ const Sidebar = () => {
                 to={`/${link.name}`} 
                 key={link.name} 
                 onClick={handleCloseSidebar}
+                style={({ isActive }) => ({
+                  backgroundColor: isActive ?
+                  currentColor : ''
+                })}
                 className={({ isActive }) => isActive ? activeLink : normalLink}>
                   {link.icon}
                   <span className="capitalize">
