f5f1acd371f1b0f3c67ca31785913ecb2aa1d845
dave_id <djanur405@gmail.com>
2022-06-23 14:30:05 +0800

    charts (done)

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, RangeColorSettingsDirective, RangeColorSettingDirective } from '@syncfusion/ej2-react-charts';
+
+import { colorMappingData, ColorMappingPrimaryXAxis, ColorMappingPrimaryYAxis, rangeColorMapping } from '../../data/dummy';
+import { ChartsHeader } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const ColorMapping = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>ColorMapping</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-lg rounded-3xl">
+      <ChartsHeader category="Color Mapping" title="INA CLIMATE - WEATHER BY MONTH" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={ColorMappingPrimaryXAxis}
+          primaryYAxis={ColorMappingPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+          legendSettings={{ background: 'white', mode: 'Range' }}
+        >
+          <Inject services={[ColumnSeries, Legend, Tooltip, Category]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={colorMappingData[0]}
+              name="INA"
+              xName="x"
+              yName="y"
+              type="Column"
+              cornerRadius={{
+                toleft: 10,
+                topRight: 10,
+              }}
+            />
+          </SeriesCollectionDirective>
+          <RangeColorSettingsDirective>
+            {rangeColorMapping.map((item, index) => <RangeColorSettingDirective key={index} {...item} />)}
+          </RangeColorSettingsDirective>
+        </ChartComponent>
+      </div>
+    </div>
   )
 }
 
-export default ColorMapping
+export default ColorMapping;
